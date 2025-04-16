bb8f318696d16855e8a46362b649596c91832450
LuieR <rhluisenrique@gmail.com>
2025-04-15 22:00:17 -0400

    chore: implemented Read and Delete, pending update controler.

--- config/routes.config.js
+++ config/routes.config.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const book = require("../controllers/book.controllers")
 
 router.post('/books', book.create)
+router.get("/books", book.list)
+router.delete("/books/:id", book.delete)
 
 module.exports = router
--- controllers/book.controllers.js
+++ controllers/book.controllers.js
@@ -1,9 +1,33 @@
 const Book = require("../models/book.model")
+const createError = require("http-errors")
 
 module.exports.create = (req, res, next) => {
 
   Book.create(req.body)
     .then(book => res.status(201).json(book))
     .catch(error => next(error))
+
+}
+
+module.exports.list = (req, res, next) => {
+
+  Book.find()
+    .then((books) => res.status(200).json(books))
+    .catch((error) => next(error))
+
+}
+
+module.exports.delete = (req, res, next) => {
+
+  Book.findById(req.params.id)
+    .then((book) => {
+      if(!book){
+        next(createError(404, "Book not found"))
+      } else {
+        return Book.findByIdAndDelete(book.id)
+                .then(() => res.status(200).send())
+      }
+    })
+    .catch((err) => next(err))
 
 }
